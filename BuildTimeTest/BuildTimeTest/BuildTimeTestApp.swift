0f3ba32bf434835e105568e9377376e92411d5d8
Lubos Lehota <lubos.lehota@gmail.com>
2025-05-11 08:38:35 +0300

    Initial commit

---
+++ BuildTimeTest/BuildTimeTest/BuildTimeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTimeTestApp.swift
+//  BuildTimeTest
+//
+//  Created by Lubos Lehota on 11/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BuildTimeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
